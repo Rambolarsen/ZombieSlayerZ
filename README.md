89a8c5f74285794ae07b1c445112254643ad3b02
Lars-Erik Arnesen <lars-erik.arnesen@bouvet.no>
2020-09-20 13:30:35 +0200

    Initial checkin

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # ZombieSlayerZ
 Project for showing off different unit test approaches
+
+## BouvetOne
+This project is ment as an appendix to the workshop had on BouvetOne fall 2020. 
---
+++ ZombieSlayerZ/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ZombieSlayerZ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
